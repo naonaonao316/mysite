2f8801b8832bb112abb1da30da760ebbe8363692
Nao Takegawa <nao.takegawa316@gmail.com>
2017-08-18 18:04:30 +0900

    add basic header and footer

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.template import Context, loader
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello,world.")
+  template = loader.get_template("dashboard/index.html")
+  return HttpResponse(template.render())
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
