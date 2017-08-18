52d26a959cc81ceba3743b6efcffdb0a50f9e8c7
Nao Takegawa <nao.takegawa316@gmail.com>
2017-08-18 13:32:24 +0900

    create 'dashboard'

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
 ]
